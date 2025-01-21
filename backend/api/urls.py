408428b3b4a545ff536d98d3236419a6c48a83fb
Ihor D <ihor.davyydenko@gmail.com>
2025-01-21 21:32:30 +0100

    rename product EP, delete redundant EPs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,5 @@ from api.views import *
 
 
 urlpatterns = [
-    path('products_list/', ListProductView.as_view(), name='products'),
-    path('categories_list/', ListCategoryView.as_view(), name='categories'),
-    path('image_add/', AddProductImageView.as_view(), name='image_add')
+    path('products/', ListProductView.as_view(), name='products')
 ]
