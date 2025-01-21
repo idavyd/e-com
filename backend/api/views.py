76e2f60fe134cbd979fba761c8640787e24c0c5f
Ihor D <ihor.davyydenko@gmail.com>
2025-01-21 21:33:14 +0100

    update Product view + delete redundant views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,9 @@
-from django.shortcuts import render
 from rest_framework import generics
 from api.serializers import *
-from django.contrib.auth.models import User
 from store.models import Category, Product
 
 
-class ListProductView(generics.ListCreateAPIView):
+class ListProductView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-
-
-class ListCategoryView(generics.ListAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-
-
-class AddProductImageView(generics.CreateAPIView):
-    serializer_class = ProductImageSerializer
-    queryset = ProductImage.objects.all()
 
